6e209a17ea2cc288d176c87c2dd7da0c8be18c35
Diana Kutubidze <diana.kutubidze@gmail.com>
2021-05-21 14:24:14 +0400

    added  console

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 if(number % 2 == 0) {
     console.log("The number is even.");
 }
+var num =5
+            document.write("number = "+num+"<br>")
+            if(num %2 ==0){
+                document.write('This number is even')
+            }else{
+                document.write('This number is false')
+            }
